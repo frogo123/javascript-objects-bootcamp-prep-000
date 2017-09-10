51ffd029565808f70fa0fe388ea3b0f682d357a9
Eddie Fisher <eddief28@tiscali.co.uk>
2017-09-10 15:43:12 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,8 +5,10 @@ var playlist = {
 
 function updatePlaylist(obj, artist, song){
   obj[artist] = song;
+  return obj;
 }
 
 function removeFromPlaylist(obj, artist){
   delete obj[artist];
+  return obj;
 }
