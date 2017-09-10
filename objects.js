f539956c79bb132368f7c1e23d05c259e67d6f47
Eddie Fisher <eddief28@tiscali.co.uk>
2017-09-10 15:39:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,11 @@ var playlist = {
   "Jamie T": "Shela",
   "Rat Boy": "Scum"
 }
+
+function updatePlaylist(obj, artist, song){
+  obj.artist = song;
+}
+
+function removeFromPlaylist(obj, artist){
+  delete obj.artist;
+}
